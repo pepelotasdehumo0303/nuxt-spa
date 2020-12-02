dc7363a3eb40a8d6000964944701532593df5d2c
pepelotasdehumo0303 <pepelotasdehumo0303@gmail.com>
2020-12-02 11:30:03 -0300

    change

--- pages/index.vue
+++ pages/index.vue
@@ -17,7 +17,9 @@ export default {
   async asyncData({$axios}) {
     const data = await $axios.$get('https://applicationnotes.000webhostapp.com/api/notes')
     console.log(data)
-    
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(json => console.log(json))
   }
 }
 </script>
