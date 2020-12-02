3956814d8dd8692ca77b6dae4dd629c9eb079d1b
pepelotasdehumo0303 <pepelotasdehumo0303@gmail.com>
2020-12-02 10:28:22 -0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="section">
+    
+  </section>
+</template>
+
+<script>
+import Card from '~/components/Card'
+
+export default {
+  name: 'HomePage',
+
+  components: {
+    Card
+  },
+
+  async asyncData({$axios}) {
+    const data = await $axios.$get('https://applicationnotes.000webhostapp.com/api/notes')
+    console.log(data)
+  }
+}
+</script>
