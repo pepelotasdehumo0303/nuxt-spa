9e20c430d72fc8902f477c0720cc7cc11488f791
pepelotasdehumo0303 <pepelotasdehumo0303@gmail.com>
2020-12-23 15:32:55 -0300

    functions

--- my_functions/hello.js
+++ my_functions/hello.js
@@ -1,6 +1,12 @@
 // this uses the callback syntax, however, we encourage you to try the async/await syntax shown in async-dadjoke.js
-exports.handler = async (event, context) => { return { statusCode: 200, 
+exports.handler = async (event, context) => { 
+  console.log(event)
+  console.log(context)
+  return { 
+    statusCode: 200,
   
-  body: JSON.stringify({message: "Hello World"})
+    body: JSON.stringify({message: "Hello World"})
 
-}; } 
+  }
+
+} 
