6177c5c8dfa0a021efdaab9462ac393e280b86ec
pepelotasdehumo0303 <pepelotasdehumo0303@gmail.com>
2020-12-23 15:20:22 -0300

    functions

--- my_functions/hello.js
+++ my_functions/hello.js
@@ -1,8 +1,2 @@
 // this uses the callback syntax, however, we encourage you to try the async/await syntax shown in async-dadjoke.js
-export function handler(event, context) {
-  console.log('probando la funcion')
-  return {
-    statusCode: 200,
-    body: JSON.stringify({message: "Hello World"})
-};
-}
+exports.handler = async (event, context) => { return { statusCode: 200, body: ' We are now split testing! ' }; } 
